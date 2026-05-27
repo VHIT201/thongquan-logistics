b0f85f60a64042f2b23debf9160a3be25b0596a1
sinh <hothiensinh03@gmail.com>
2026-05-27 14:37:48 +0700

    Add pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
       script: "node_modules/.bin/next",
       args: "start",
       env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
         NODE_ENV: "production",
       },
     },
